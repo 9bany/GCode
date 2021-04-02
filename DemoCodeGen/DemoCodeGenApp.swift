74e1e4ae9bd7dd9991a5ffd58c7c862a01883380
eban <eban.y>
2021-04-02 11:28:45 +0700

    Initial Commit

---
+++ DemoCodeGen/DemoCodeGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCodeGenApp.swift
+//  DemoCodeGen
+//
+//  Created by eban on 02/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoCodeGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
